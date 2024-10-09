4c18eb2aed3aa2a8fbf84360df7a3939a152ca95
Viacheslav Mykhailov <viacheslav.leo.mykhailov@gmail.com>
2024-10-09 23:55:10 +0300

    change comments in the fibonacci task

--- task01/memoization_fibonacci.py
+++ task01/memoization_fibonacci.py
@@ -1,5 +1,5 @@
 def caching_fibonacci() -> int:
-    def fibonacci(n: int)-> int: 
+    def fibonacci(n: int)-> int:
         '''Calculates n-th fibonacci number with memoization technique
         ARGS: n: n-th fibonacci number
         RETURNS: Value of the n-th fibonacci number  
@@ -11,9 +11,9 @@ def caching_fibonacci() -> int:
         if n == 1:
             return 1
         
-        if n not in cache:  # if n number is already in 'cache' dictionary, return its value
+        if n not in cache:  # if n number isn't yet in cache, calculates it by the formula and save it
             cache[n] = fibonacci(n - 1) + fibonacci(n - 2)
-        # if not, calculates it by the formula
+        # if not, immediately return its value
         return cache[n]
 
     return fibonacci
