95efaca692f78c09c77221e8eaad8f3b78398e08
Viacheslav Mykhailov <viacheslav.leo.mykhailov@gmail.com>
2024-10-09 21:57:17 +0300

    add method of finding n-th fibonacci number with inner function and memoization technique

---
+++ task01/memoization_fibonacci.py
@@ -0,0 +1,26 @@
+def caching_fibonacci() -> int:
+    def fibonacci(n):
+        '''Calculates n-th fibonacci number with memoization technique
+        ARGS: n: n-th fibonacci number
+        RETURNS: Value of the n-th fibonacci number  
+        '''
+        cache = {}
+        if n <= 0:
+            return 0
+
+        if n == 1:
+            return 1
+        
+        if n not in cache:  # if n number is already in 'cache' dictionary, return its value
+            cache[n] = fibonacci(n - 1) + fibonacci(n - 2)
+        # if not, calculates it by the formula
+        return cache[n]
+
+    return fibonacci
+
+
+fib = caching_fibonacci()
+print(fib(5))
+print(fib(10))
+print(fib(15))
+print(fib(32))
